b7dd306b0bded5921cb2e4516d8886849464443f
wanjihui <2696658291@qq.com>
2026-05-21 15:16:31 +0800

    真最终版，修复spi时序问题，最大值4095

--- adc.c
+++ adc.c
@@ -11,40 +11,36 @@ unsigned int adc_Read(void)
     EA = 0;
 
     adc_CS = 0;
+    adc_DCLK = 0;
 
     // 第一步：发送8位控制字
     for (i = 0; i < 8; i++)
     {
-        adc_DCLK = 0;
         adc_DIN = (cmd >> (7 - i)) & 0x01;
-        _nop_();
-        _nop_();
         adc_DCLK = 1;
         _nop_();
+			  adc_DCLK = 0;
         _nop_();
     }
-
-    // 关键 跳过1个无效的BUSY位时钟
-    adc_DCLK = 0;
-    _nop_();
+		//跳过忙线
+		adc_DCLK = 0;
     _nop_();
     adc_DCLK = 1;
     _nop_();
+		
+    adc_DCLK = 0;
     _nop_();
-
-    //读取真正的12位有效数据
+    //读取12位数据
     for (i = 0; i < 12; i++)
     {
-        adc_DCLK = 0;
-        _nop_();
-        _nop_();
         value <<= 1;
-        if (adc_DOUT) value |= 0x01;
         adc_DCLK = 1;
         _nop_();
+				adc_DCLK = 0;
         _nop_();
+        if (adc_DOUT) value |= 0x01;
     }
-
+		
     adc_CS = 1;
     EA = ea_save;
 
